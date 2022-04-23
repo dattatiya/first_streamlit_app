fbc4474ca6bfaaf0e09263c7131b5ecf6ca6f70b
Tiyasha Datta <104169423+dattatiya@users.noreply.github.com>
2022-04-23 17:51:18 +0100

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header('Fruityvice Fruit Advice !')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
+fruity_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered', fruity_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruity_choice)
